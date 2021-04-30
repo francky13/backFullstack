db6742e337718ed5a1288567499eb2ea6fef0327
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-30 17:24:43 +0300

    a

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('routes'));
+app.use(express.static(__dirname + "/web"));
+var SuiviRoute = require('./routes/SuiviRoute');
+app.use('/', SuiviRoute);
 
 server.listen(PORT, function() {
   console.log('Chat server running');
+   const all_routes = require('express-list-endpoints');
+    console.log(all_routes(app));
 });
 
-var SuiviRoute = require('./routes/SuiviRoute');
 
-app.use('/', SuiviRoute);
 
 
