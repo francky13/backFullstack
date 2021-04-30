8b2220f295c07385e044b8847b2324206f59cb81
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-30 17:01:38 +0300

    x

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('routes'));
-
-
-
+app.use(express.static('client'));
 
 server.listen(PORT, function() {
-  console.log('Chat server running'+ PORT);
-  const all_routes = require('express-list-endpoints');
-  console.log(all_routes(app));
+  console.log('Chat server running');
 });
-
 
 var io = require('socket.io')(server);
 
@@ -24,4 +18,3 @@ io.on('connection', function(socket) {
     io.emit('message', msg);
   });
 });
-
