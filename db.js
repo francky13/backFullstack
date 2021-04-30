4c71a6b39fc5bc3754b57421fe652568d8b3d107
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-30 15:16:01 +0300

    k

--- db.js
+++
@@ -1,9 +0,0 @@
-var mysqlModel = require('mysql');
-let connection = mysqlModel.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'fullstackcovis'
-});
-module.exports = connection;
-
--- server.js
+++
@@ -1,7 +0,0 @@
-let app = require('./app');
-let port = process.env.PORT || 8080;
-let server = app.listen(port, function () {
-    console.log('Express server listening on port ' + port);
-    const all_routes = require('express-list-endpoints');
-    console.log(all_routes(app));
-});
