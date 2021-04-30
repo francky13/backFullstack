d9cc52d34061ee329660e7d9a6d213ef59b1cb59
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-30 15:24:03 +0300

    pff

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-onst express = require('express');
-const app = express();
-const port = 3000;
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var http = require('http');
+var server = http.Server(app);
 
-app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  socket.on('message', function(msg) {
+    io.emit('message', msg);
+  });
+});
