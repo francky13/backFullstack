c00eedb4747b0e46d7446c5d30d63e667f998219
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-30 16:49:52 +0300

    c

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 8080;
 var express = require('express');
 var app = express();
 
 var http = require('http');
 var server = http.Server(app);
 
+app.use(express.static('routes'));
 
-app.use(express.static(__dirname + "/web"));
+var SuiviRoute = require('./routes/SuiviRoute');
+app.use('/api/suivi', SuiviRoute);
 
 
 server.listen(PORT, function() {
-  console.log('Chat server running');
+  console.log('Chat server running'+ PORT);
   const all_routes = require('express-list-endpoints');
   console.log(all_routes(app));
 });
 
-var io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
