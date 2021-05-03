2f59bf67194884712386f1c5aac0a5d1bc8c29a1
francky13 <rabemiakatrafrancky@gmail.com>
2021-05-03 10:43:28 +0300

    tsss

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
-let cors = require('cors');
+
 var http = require('http');
 var server = http.Server(app);
-app.use(cors({ origin: "http://localhost:5000/" }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+
 
 app.use(express.static(__dirname + "/web"));
 var SuiviRoute = require('./routes/SuiviRoute');
-var authRoute = require('./routes/auth');
-var InfoRoute = require('./routes/inforoute');
 
-app.use('/api/auth', authRoute);
-app.use('/api/suivi', SuiviRoute);
-app.use('/api/info', InfoRoute);
+
+
+app.use('/', SuiviRoute);
+
 
 
 server.listen(PORT, function() {
